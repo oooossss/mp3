d0149577dcee0011f6984cc8b05db52b49536f52
oooossss <heqianleapply@163.com>
2015-11-02 12:06:52 -0500

    Update event.h

--- event.h
+++ event.h
@@ -1,17 +1,53 @@
-#include <linux/kernel.h>
-#include <linux/slab.h>
-#include <linux/list.h>
-#include <linux/types.h>
-#include <linux/wait.h>
-#include <linux/sched.h>
+/* linux/include/linux/eventcalls.h */
 
-struct event 
+#ifndef __EVENT_H
+#define __EVENT_H
+
+#include <linux/kernel_stat.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/percpu.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/swap.h>
+#include <linux/pid_namespace.h>
+#include <linux/notifier.h>
+#include <linux/thread_info.h>
+#include <linux/time.h>
+#include <linux/jiffies.h>
+#include <linux/posix-timers.h>
+#include <linux/cpu.h>
+#include <linux/syscalls.h>
+#include <linux/delay.h>
+#include <linux/tick.h>
+#include <linux/kallsyms.h>
+
+#include <asm/uaccess.h>
+#include <asm/unistd.h>
+#include <asm/div64.h>
+#include <asm/timex.h>
+#include <asm/io.h>
+
+#include <linux/kernel.h>
+
+#include <linux/sched.h>   
+#include <linux/list.h> 
+#include <linux/wait.h>
+#include <linux/spinlock.h>
+#include <linux/types.h>
+
+struct event
 {
-	int id;
-	uid_t uid;
-	gid_t gid;
-	int uidflag;
-	int gidflag;
-	struct list_head eventID_list;
-	wait_queue_head_t wait_queue;
-}
+    uid_t UID;
+    gid_t GID;
+    int UIDFlag;
+    int GIDFlag;
+    /* eventID should be positive integers. */
+    int eventID;
+    
+    /* Implement a kernel double-linked list fo events. */
+    struct list_head eventID_list;
+    
+    /* Implement a wait queue of processes waiting on the event. */
+    wait_queue_head_t wait_queue;
+};
